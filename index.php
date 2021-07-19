7bc7051bd9bfbb73f8a8b2726862e690bfce7f7a
Chino Concepcion <cconcepcion02@gmail.com>
2021-07-19 08:48:09 +0800

    sample commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+$string = "";
+
+$string .= "name";
+$string .= "lastname";
+echo $string;
+?>
+
+
+<html>
+<head>
+<title >Title of the document<title>
+<script src="jscript.js"><script>
+</head>
+
+<body>
+<p id="demo">sss </p>
+
+
+</body>
+
+<html>
+
+<script>222
+	document.getElementById(demo).innerHTML = text;
+</script>
---
+++ jscript.js
@@ -0,0 +1,58 @@
+
+
+(function test(){console.log("test")})();
+	{
+			let str = [
+				{
+					name:"Chino",
+					lastname:"Concepcion",
+					age:20,
+					likes:["volleyball"],
+					wants:
+						{
+							phone:'samsung',
+							laptop:'acer'
+						}
+				},
+				{
+					name:"Chino1",
+					lastname:"Concepcion1",
+					age:201,
+					likes:["computer","cellphone"],
+					wants:
+						{
+							phone:'samsung',
+							laptop:'acer'
+						}
+				},
+				{
+					name:"Chino2",
+					lastname:"Concepcion1",
+					age:201,
+					likes:["computer","cellphone"],
+					wants:
+						{
+							phone:'samsung',
+							laptop:'acer'
+						}
+				}
+				
+				];
+				console.log(str);
+			var text = "";
+ 			for(var i = 0; i < str.length; i++)
+			{			
+				text += "<br>" +' '+ str[i].name +' '+ str[i].lastname +' '+ str[i].age +' '+ str[i].likes +' '+ str[i].wants.phone +' '+ str[i].wants.laptop +' ';
+					for(var j=0;j<str[i].likes.length;j++)
+					{
+						if(j<1){
+						text += str[i].likes[j] + "";
+						}else{
+						text += " and " + str[i].likes[j];
+						}
+				
+					}				
+			} 
+			
+	}
+
